b3775edcc383fde2eca7fa4de3a1345dd082b739
Steeves ON <steevesobiang@gmail.com>
2022-11-14 14:38:53 +0100

    add random animation and fixed layout

--- src/components/display_messages/display_messages.ts
+++ src/components/display_messages/display_messages.ts
@@ -7,6 +7,8 @@ import { Router } from 'aurelia-router';
 export class display_messages{
   private url = "https://wish-ipsw.onetec.eu/api/wish.php"
   private allMessages;
+  private wrapper:HTMLDivElement
+  private bubbleMessages:Array<HTMLDivElement> = [];
   
   constructor(private router:Router){
     //this.getMessage().then(()=>{this.loadData();});
@@ -14,9 +16,27 @@ export class display_messages{
     this.reloadPage();
   }
 
+  public activate() {
+  }
+
+  public bind() {
+    //console.log("wrapper = "+this.wrapper);
+     // $("#wrapper").addClass(classes.splice(~~(Math.random()*classes.length), 1 )[0] );
+     /* $(".bublleMessage").each(function(){
+        $(this).addClass(classes[~~(Math.random()*classes.length)]);
+    });*/
+    //console.log("getRandomClass = "+this.getRandomClass())
+
+  }
+
+  public getRandomClass(){
+    var classes = ["floating", "floatingLR"];
+    return classes[Math.floor(Math.random()*classes.length)];
+  }
+
+
   public reloadPage(){
     setInterval(() => {
-      //console.log('reload');
       window.location.reload();
       }, 60 * 1000);
   }
@@ -24,17 +44,19 @@ export class display_messages{
   async getMessage() {
     const http = new HttpClient();
     await http.fetch(this.url)
-    .then(resp => resp.json())
-    .then(data => {
-    this.allMessages = data;
-    localStorage.setItem('messages', JSON.stringify(this.allMessages));
-    console.log('Data fetched = '+this.allMessages)
-    return data
-    })
-    .catch(error => {
-      this.allMessages = JSON.parse(localStorage.getItem('messages')) || [];
-      console.log(error)
-    })
+      .then(resp => resp.json())
+      .then(data => {
+      this.allMessages = data;
+      localStorage.setItem('messages', JSON.stringify(this.allMessages));
+      console.log('Data fetched = '+this.allMessages)
+      return data
+      })
+      .catch(error => {
+        this.allMessages = JSON.parse(localStorage.getItem('messages')) || [];
+        console.log(error)
+      })
+      
+    //console.log("Bubble Messages = "+this.bubbleMessages[0]);
 }  
  
 }
